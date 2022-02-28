8f8be8d334bffdcf81161bd72b87f48bc52687db
FOXYSAMU <39921257+FOXYSAMU@users.noreply.github.com>
2022-02-27 21:03:00 -0500

    Completed first part of rock paper scissors project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
 let game = ['Rock', 'Paper', 'Scissors'] // rock paper scissors options
+let playerScore = 0;
+let computerScore = 0;
+const computerSelection = computerPlay();
+const playerSelection = prompt('Rock Paper or Scissors???');
 
 function computerPlay(){
     const randomChoice = game[Math.floor(Math.random() * game.length)]; 
@@ -10,26 +14,30 @@ function gameRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
 
     if (playerSelection === 'ROCK' && computerSelection === 'Scissors'){
-        console.log('You Win!');
+        return ('You Win!');
     }
     else if (playerSelection === 'ROCK' && computerSelection === 'Paper'){
-        console.log('You lost...');
+        return ('You lost...');
     }
     else if (playerSelection === 'ROCK' && computerSelection === 'Rock'){
-        console.log('It\'s a tie');
+        return ('It\'s a tie');
     }
     else {
-        console.log('Something went wrong :(')
+        return ('Something went wrong :(')
     }
 }
 
 function gameMatch(){
-    for (let i = 0; i < 5; i++) {
-        gameRound()
-     }
+    if (eachRound === 'You Win!'){
+        playerScore++
+    }
+    else if(eachRound === 'You lost...'){
+        computerScore++
+    }
 }
 
-const computerSelection = computerPlay();
-const playerSelection = 'rock';
-console.log(gameRound(playerSelection, computerSelection));
 
+let eachRound = gameRound(playerSelection, computerSelection);
+gameMatch()
+console.log(eachRound)
+console.log(playerScore)
